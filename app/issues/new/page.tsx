c861daf32b1fa34f3c55f9ca74407754bb374459
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-29 17:49:34 +0900

    Integrate EasyMDE markdown editor library for rich text editing in new issue creation, update package dependencies for Easymde and react-simplemde-editor, and refactor NewIssuePage to utilize SimpleMDE for description input.

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,11 +1,13 @@
 'use client';
-import { Button, TextArea, TextField } from '@radix-ui/themes';
+import { Button, TextField } from '@radix-ui/themes';
+import 'easymde/dist/easymde.min.css';
+import SimpleMDE from 'react-simplemde-editor';
 
 const NewIssuePage = () => {
   return (
     <div className="max-w-xl space-y-3">
       <TextField.Root placeholder="Title"></TextField.Root>
-      <TextArea placeholder="Description"></TextArea>
+      <SimpleMDE placeholder="Description"></SimpleMDE>
       <Button>Submit New Issue</Button>
     </div>
   );
