162e41aca49c7226ecb22d7b38d9b80669933d55
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-29 22:59:49 +0900

    Add @hookform/resolvers dependency, create validation schema for issue creation, and refactor NewIssuePage to utilize Zod for form validation.

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,25 +1,31 @@
 'use client';
-import { Button, Callout, TextField } from '@radix-ui/themes';
-
+import { createIssueSchema } from '@/app/validationSchemas';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button, Callout, Text, TextField } from '@radix-ui/themes';
 import axios from 'axios';
 import 'easymde/dist/easymde.min.css';
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
+import { z } from 'zod';
 
 const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
   ssr: false,
 });
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, handleSubmit, control } = useForm<IssueForm>();
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const [error, setError] = useState('');
   return (
     <div className="max-w-xl">
@@ -40,11 +46,21 @@ const NewIssuePage = () => {
         className="space-y-3"
       >
         <TextField.Root placeholder="Title" {...register('title')} />
+        {errors.title && (
+          <Text color="red" as="p" mb="5">
+            {errors.title.message}
+          </Text>
+        )}
         <Controller
           control={control}
           name="description"
           render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
         />
+        {errors.description && (
+          <Text color="red" as="p" mb="5">
+            {errors.description.message}
+          </Text>
+        )}
         <Button>Submit New Issue</Button>
       </form>
     </div>
