7a84ed77c6cd268bb8e3d3939b1d072bf674ab00
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-29 21:55:19 +0900

    Add axios and react-hook-form dependencies, refactor NewIssuePage to handle form submission with axios and react-hook-form, and update package-lock.json accordingly.

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,15 +1,39 @@
 'use client';
 import { Button, TextField } from '@radix-ui/themes';
+import axios from 'axios';
 import 'easymde/dist/easymde.min.css';
-import SimpleMDE from 'react-simplemde-editor';
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/navigation';
+import { Controller, useForm } from 'react-hook-form';
+
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
+  ssr: false,
+});
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, handleSubmit, control } = useForm<IssueForm>();
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Title"></TextField.Root>
-      <SimpleMDE placeholder="Description"></SimpleMDE>
+    <form
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post('/api/issues', data);
+        router.push('/issues');
+      })}
+      className="max-w-xl space-y-3"
+    >
+      <TextField.Root placeholder="Title" {...register('title')} />
+      <Controller
+        control={control}
+        name="description"
+        render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
+      />
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
