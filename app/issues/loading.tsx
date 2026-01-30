e5727560662fafa6bf382a3124e4e3f0f72cc0c0
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-30 18:13:52 +0900

    Update package dependencies by adding 'delay' and 'react-loading-skeleton' for enhanced functionality, and refactor IssuesPage to remove the New Issue button in favor of a dedicated IssueActions component for improved UI structure.

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,10 +1,10 @@
 import { Table } from '@radix-ui/themes';
-import { prisma } from '../../prisma/client';
-import IssueStatusBadge from '../components/IssueStatusBadge';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 import IssueActions from './IssueActions';
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
+const LoadingIssuesPage = () => {
+  const issues = [1, 2, 3, 4, 5];
   return (
     <div>
       <IssueActions />
@@ -21,18 +21,18 @@ const IssuesPage = async () => {
 
         <Table.Body>
           {issues.map((issue) => (
-            <Table.Row key={issue.id}>
+            <Table.Row key={issue}>
               <Table.RowHeaderCell>
-                {issue.title}
+                <Skeleton />
                 <div className="block md:hidden">
-                  <IssueStatusBadge status={issue.status} />
+                  <Skeleton />
                 </div>
               </Table.RowHeaderCell>
               <Table.Cell className="hidden md:table-cell">
-                <IssueStatusBadge status={issue.status} />
+                <Skeleton />
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
-                {issue.createdAt.toDateString()}
+                <Skeleton />
               </Table.Cell>
             </Table.Row>
           ))}
@@ -42,4 +42,4 @@ const IssuesPage = async () => {
   );
 };
 
-export default IssuesPage;
+export default LoadingIssuesPage;
