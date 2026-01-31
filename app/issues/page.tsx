5f868ed3fef87fdbc584a7d2c6be5eec5cba6edb
yuta asakura <yuta.asakura.se@gmail.com>
2026-01-31 15:49:06 +0900

    Organize import statements and add Skeleton component and refactor imports in loading pages to use centralized components for improved code organization and consistency.

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,6 @@
 import { Table } from '@radix-ui/themes';
 import { prisma } from '../../prisma/client';
-import IssueStatusBadge from '../components/IssueStatusBadge';
-import Link from '../components/Link';
+import { IssueStatusBadge, Link } from '@/app/components';
 import IssueActions from './IssueActions';
 
 const IssuesPage = async () => {
