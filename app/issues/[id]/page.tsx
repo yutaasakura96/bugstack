2d1620785712af7219b74dc09251673f59a2f056
yuta asakura <yuta.asakura.se@gmail.com>
2026-02-02 15:20:09 +0900

    Add @radix-ui/react-icons dependency and update IssueDetailPage layout to include edit button and icon for improved user interaction.

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,4 +1,6 @@
-import { Card, Flex, Heading, Text } from '@radix-ui/themes';
+import { Pencil2Icon } from '@radix-ui/react-icons';
+import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import ReactMarkdown from 'react-markdown';
 import { prisma } from '../../../prisma/client';
@@ -24,16 +26,23 @@ const IssueDetailPage = async ({ params }: Props) => {
   }
 
   return (
-    <div>
-      <Heading>{issue?.title}</Heading>
-      <Flex gap="4" my="2">
-        <IssueStatusBadge status={issue.status} />
-        <Text>{issue.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card mt="4" className="prose">
-        <ReactMarkdown>{issue?.description}</ReactMarkdown>
-      </Card>
-    </div>
+    <Grid columns={{ initial: '1', md: '2' }} gap="5">
+      <Box>
+        <Heading>{issue?.title}</Heading>
+        <Flex gap="4" my="2">
+          <IssueStatusBadge status={issue.status} />
+          <Text>{issue.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card mt="4" className="prose">
+          <ReactMarkdown>{issue?.description}</ReactMarkdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon /> <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
