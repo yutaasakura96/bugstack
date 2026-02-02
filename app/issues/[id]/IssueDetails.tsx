8adf358f825baef86cd5cd2f6d5adad0d7747c11
yuta asakura <yuta.asakura.se@gmail.com>
2026-02-02 16:16:58 +0900

    Apply Single Resposinbility Principle; Add EditIssueButton and IssueDetails components to enhance issue detail view with editing functionality and improved layout.

---
+++ app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,21 @@
+import { Card, Flex, Heading, Text } from '@radix-ui/themes';
+import ReactMarkdown from 'react-markdown';
+import { Issue } from '../../../generated/prisma/client';
+import IssueStatusBadge from '../../components/IssueStatusBadge';
+
+const IssueDetails = ({ issue }: { issue: Issue }) => {
+  return (
+    <>
+      <Heading>{issue?.title}</Heading>
+      <Flex gap="4" my="2">
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card mt="4" className="prose">
+        <ReactMarkdown>{issue?.description}</ReactMarkdown>
+      </Card>
+    </>
+  );
+};
+
+export default IssueDetails;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,10 +1,8 @@
-import { Pencil2Icon } from '@radix-ui/react-icons';
-import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes';
-import Link from 'next/link';
+import { Box, Grid } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
-import ReactMarkdown from 'react-markdown';
 import { prisma } from '../../../prisma/client';
-import IssueStatusBadge from '../../components/IssueStatusBadge';
+import EditIssueButton from './EditIssueButton';
+import IssueDetails from './IssueDetails';
 
 interface Props {
   params: Promise<{ id: string }>;
@@ -28,19 +26,10 @@ const IssueDetailPage = async ({ params }: Props) => {
   return (
     <Grid columns={{ initial: '1', md: '2' }} gap="5">
       <Box>
-        <Heading>{issue?.title}</Heading>
-        <Flex gap="4" my="2">
-          <IssueStatusBadge status={issue.status} />
-          <Text>{issue.createdAt.toDateString()}</Text>
-        </Flex>
-        <Card mt="4" className="prose">
-          <ReactMarkdown>{issue?.description}</ReactMarkdown>
-        </Card>
+        <IssueDetails issue={issue} />
       </Box>
       <Box>
-        <Button>
-          <Pencil2Icon /> <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
-        </Button>
+        <EditIssueButton issueId={issue.id} />
       </Box>
     </Grid>
   );
