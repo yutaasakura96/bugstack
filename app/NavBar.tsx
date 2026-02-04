fb916e0e1ad003ba470a1dfe05917221ee66e46b
yuta asakura <yuta.asakura.se@gmail.com>
2026-02-04 22:29:57 +0900

    Enhance NavBar component by integrating DropdownMenu for user authentication; replace logout link with an avatar trigger that displays user email and logout option, improving user experience and interface design.

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Box, Container, Flex } from '@radix-ui/themes';
+import { Avatar, Box, Container, DropdownMenu, Flex, Text } from '@radix-ui/themes';
 import classnames from 'classnames';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
@@ -46,7 +46,28 @@ const NavBar = () => {
             </ul>
           </Flex>
           <Box>
-            {status === 'authenticated' && <Link href="/api/auth/signout">Logout</Link>}
+            {status === 'authenticated' && (
+              // <Link href="/api/auth/signout">Logout</Link
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    src={session.user!.image!}
+                    fallback="?"
+                    size="2"
+                    radius="full"
+                    className="cursor-pointer"
+                  />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                  <DropdownMenu.Label>
+                    <Text size="2">{session.user!.email}</Text>
+                  </DropdownMenu.Label>
+                  <DropdownMenu.Item asChild>
+                    <Link href="/api/auth/signout">Logout</Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
+            )}
             {status === 'unauthenticated' && <Link href="/api/auth/signin">Login</Link>}
           </Box>
         </Flex>
