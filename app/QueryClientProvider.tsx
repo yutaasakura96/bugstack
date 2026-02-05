d0e96a5d5d19a4e095b35ca1fb653707108faf6b
yuta asakura <yuta.asakura.se@gmail.com>
2026-02-05 14:58:36 +0900

    Integrate @tanstack/react-query for data fetching and add QueryClientProvider
    - Added @tanstack/react-query and related packages to manage server state and data fetching.
    - Created a new QueryClientProvider component to encapsulate the QueryClient setup.
    - Updated RootLayout to include QueryClientProvider and ReactQueryDevtools for enhanced debugging capabilities.

---
+++ app/QueryClientProvider.tsx
@@ -0,0 +1,13 @@
+'use client';
+import {
+  QueryClient,
+  QueryClientProvider as ReactQueryClientProvider,
+} from '@tanstack/react-query';
+import { PropsWithChildren } from 'react';
+
+const QueryClientProvider = ({ children }: PropsWithChildren) => {
+  const queryClient = new QueryClient();
+  return <ReactQueryClientProvider client={queryClient}>{children}</ReactQueryClientProvider>;
+};
+
+export default QueryClientProvider;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import { Container, Theme } from '@radix-ui/themes';
 import '@radix-ui/themes/styles.css';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import NavBar from './NavBar';
+import QueryClientProvider from './QueryClientProvider';
 import AuthProvider from './api/Provider';
 import './globals.css';
 import './theme-config.css';
@@ -24,14 +26,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <AuthProvider>
-          <Theme accentColor="violet">
-            <NavBar />
-            <Container>
-              <main className="p-5">{children}</main>
-            </Container>
-          </Theme>
-        </AuthProvider>
+        <QueryClientProvider>
+          <AuthProvider>
+            <Theme accentColor="violet">
+              <NavBar />
+              <Container>
+                <main className="p-5">{children}</main>
+              </Container>
+            </Theme>
+          </AuthProvider>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </body>
     </html>
   );
