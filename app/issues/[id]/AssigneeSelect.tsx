ec4f50d49d7c22bacb7a884806da06d3ca857f2d
yuta asakura <yuta.asakura.se@gmail.com>
2026-02-05 14:36:14 +0900

    Add user API endpoint and update AssigneeSelect to fetch users
    - Created a new API endpoint to retrieve a list of users from the database, ordered by name.
    - Updated the AssigneeSelect component to fetch users from the new API endpoint, enhancing the user assignment functionality by dynamically populating the selection options.

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,16 +1,31 @@
 'use client';
 import { Select } from '@radix-ui/themes';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { User } from '../../../generated/prisma/client';
 
 const AssigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await axios.get<User[]>('/api/users');
+      setUsers(data);
+    };
+    fetchUsers().catch((err) => {
+      console.error(err);
+    });
+  }, []);
   return (
-    <Select.Root defaultValue="1">
+    <Select.Root defaultValue={users[0]?.id}>
       <Select.Trigger placeholder="Assign to..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="1">user1</Select.Item>
-          <Select.Item value="2">user2</Select.Item>
-          <Select.Item value="3">user3</Select.Item>
+          {users.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
