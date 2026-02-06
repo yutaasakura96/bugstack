bc01d9d3bf1dfe089d9fc6014cf7e5922b5bc896
yuta asakura <yuta.asakura.se@gmail.com>
2026-02-06 17:33:47 +0900

    Add IssueStatusFilter component and update IssueActions layout
    - Introduced a new IssueStatusFilter component for filtering issues by status.
    - Updated IssueActions to use Flex for layout and include the new IssueStatusFilter, enhancing the user interface for issue management.

---
+++ app/issues/IssueStatusFilter.tsx
@@ -0,0 +1,29 @@
+'use client';
+import { Select } from '@radix-ui/themes';
+import { Status } from '../../generated/prisma/client';
+
+const IssueStatusFilter = () => {
+  const statuses: { label: string; value: Status }[] = [
+    { label: 'Open', value: 'OPEN' },
+    { label: 'In Progress', value: 'IN_PROGRESS' },
+    { label: 'Closed', value: 'CLOSED' },
+  ];
+
+  return (
+    <Select.Root>
+      <Select.Trigger placeholder="Filter by status..." />
+      <Select.Content>
+        <Select.Group>
+          <Select.Item value="ALL">All</Select.Item>
+          {statuses.map((status) => (
+            <Select.Item key={status.value} value={status.value}>
+              {status.label}
+            </Select.Item>
+          ))}
+        </Select.Group>
+      </Select.Content>
+    </Select.Root>
+  );
+};
+
+export default IssueStatusFilter;
