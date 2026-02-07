3ca815095d16a4231935aad00d1f8f3ba56b21ee
yuta asakura <yuta.asakura.se@gmail.com>
2026-02-07 16:05:41 +0900

    Refactor IssuesPage to support sorting and enhance table structure
    - Updated IssuesPage to accept an additional 'orderBy' parameter for sorting issues.
    - Refactored table header to dynamically generate columns and include sorting indicators.
    - Improved routing for issue navigation by integrating NextLink for better user experience.

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,15 +1,38 @@
 import { IssueStatusBadge, Link } from '@/app/components';
+import { ArrowUpIcon } from '@radix-ui/react-icons';
 import { Table } from '@radix-ui/themes';
-import { Status } from '../../../generated/prisma/client';
+import NextLink from 'next/link';
+import { Issue, Status } from '../../../generated/prisma/client';
 import { prisma } from '../../../prisma/client';
 import IssueActions from './IssueActions';
 
 interface Props {
-  searchParams: { status?: Status };
+  searchParams: { status?: Status; orderBy: keyof Issue };
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
-  const { status } = await searchParams;
+  const columns: {
+    label: string;
+    value: keyof Issue;
+    className?: string;
+  }[] = [
+    {
+      label: 'Issue',
+      value: 'title',
+    },
+    {
+      label: 'Status',
+      value: 'status',
+      className: 'hidden md:table-cell',
+    },
+    {
+      label: 'Created',
+      value: 'createdAt',
+      className: 'hidden md:table-cell',
+    },
+  ];
+
+  const { status, orderBy } = await searchParams;
   const statuses = Object.values(Status);
   const validStatus = status && statuses.includes(status) ? status : undefined;
 
@@ -18,18 +41,26 @@ const IssuesPage = async ({ searchParams }: Props) => {
       status: validStatus,
     },
   });
-
+  // TODO: Add Descending order sort
   return (
     <div>
       <IssueActions />
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Created
-            </Table.ColumnHeaderCell>
+            {columns.map((column) => (
+              <Table.ColumnHeaderCell key={column.value} className={column.className}>
+                <NextLink
+                  href={{
+                    pathname: '/issues/list',
+                    query: { status, orderBy: column.value },
+                  }}
+                >
+                  {column.label}
+                </NextLink>
+                {column.value === orderBy && <ArrowUpIcon className="inline" />}
+              </Table.ColumnHeaderCell>
+            ))}
           </Table.Row>
         </Table.Header>
 
