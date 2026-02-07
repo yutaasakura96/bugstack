ad72ac4089a238880864504d3772f5af175831dd
yuta asakura <yuta.asakura.se@gmail.com>
2026-02-07 17:48:34 +0900

    Enhance IssueStatusFilter to support dynamic query parameters
    - Updated IssueStatusFilter to utilize useSearchParams for managing URL query parameters.
    - Implemented logic to conditionally set or delete the 'status' and 'orderBy' parameters based on user selection.
    - Improved routing to ensure accurate navigation based on selected filter options.

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { Select } from '@radix-ui/themes';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { Status } from '../../../generated/prisma/client';
 
 const IssueStatusFilter = () => {
@@ -12,11 +12,23 @@ const IssueStatusFilter = () => {
   ];
 
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   return (
     <Select.Root
+      defaultValue={searchParams.get('status') ?? ''}
       onValueChange={(status) => {
-        const query = status === 'ALL' ? '' : `?status=${status}`;
+        const params = new URLSearchParams(searchParams);
+        if (status && status !== 'ALL') {
+          params.set('status', status);
+        } else {
+          params.delete('status');
+        }
+        if (searchParams.get('orderBy')) {
+          params.set('orderBy', searchParams.get('orderBy')!);
+        }
+
+        const query = params.size ? `?${params.toString()}` : '';
         router.push(`/issues/list${query}`);
       }}
     >
