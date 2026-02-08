082ceda271ef7bc5ce3f24b5666895da18ad7d4d
yuta asakura <yuta.asakura.se@gmail.com>
2026-02-08 15:01:28 +0900

    Implement Pagination component and update Home page
    - Added a new Pagination component to handle pagination logic and UI.
    - Updated the Home page to render the Pagination component with specified props for item count, page size, and current page.

---
+++ app/components/Pagination.tsx
@@ -0,0 +1,49 @@
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  DoubleArrowLeftIcon,
+  DoubleArrowRightIcon,
+} from '@radix-ui/react-icons';
+import { Button, Flex, Text } from '@radix-ui/themes';
+
+interface Props {
+  itemCount: number;
+  pageSize: number;
+  currentPage: number;
+}
+
+const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
+  const pageCount = Math.ceil(itemCount / pageSize);
+  if (pageCount <= 1) return null;
+  return (
+    <Flex justify="center" align="center" gap="2">
+      <Button color="gray" variant="soft" className="cursor-pointer" disabled={currentPage === 1}>
+        <DoubleArrowLeftIcon />
+      </Button>
+      <Button color="gray" variant="soft" className="cursor-pointer" disabled={currentPage === 1}>
+        <ChevronLeftIcon />
+      </Button>
+      <Text size="2">
+        Page {currentPage} of {pageCount}
+      </Text>
+      <Button
+        color="gray"
+        variant="soft"
+        className="cursor-pointer"
+        disabled={currentPage === pageCount}
+      >
+        <ChevronRightIcon />
+      </Button>
+      <Button
+        color="gray"
+        variant="soft"
+        className="cursor-pointer"
+        disabled={currentPage === pageCount}
+      >
+        <DoubleArrowRightIcon />
+      </Button>
+    </Flex>
+  );
+};
+
+export default Pagination;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
+import Pagination from './components/Pagination';
+
 export default function Home() {
-  return (
-    <div>BugStack</div>
-  );
+  return <Pagination itemCount={100} pageSize={10} currentPage={2} />;
 }
